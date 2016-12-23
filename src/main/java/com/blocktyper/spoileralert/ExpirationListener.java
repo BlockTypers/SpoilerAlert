fffcf0d480e74b228c49e287d71da686a6b3f72d
BuildTools <unconfigured@null.spigotmc.org>
2016-12-23 12:43:25 -0500

    fixed issue with crafted items not getting expiration dates

--- src/main/java/com/blocktyper/spoileralert/ExpirationListener.java
+++ src/main/java/com/blocktyper/spoileralert/ExpirationListener.java
@@ -36,6 +36,9 @@ public class ExpirationListener implements Listener {
 		this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 
+	/*
+	 * ON FURNACE SMELT
+	 */
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onFurnaceSmeltEvent(FurnaceSmeltEvent event) {
 		plugin.debugInfo("onFurnaceSmeltEvent");
@@ -51,11 +54,15 @@ public class ExpirationListener implements Listener {
 		// if the food is not expired set daysSourceExpired to null in case it
 		// is 0, we want setExpirationDate() to ignore it
 		Long daysSourceExpired = getDaysExpired(event.getSource(), world);
-		daysSourceExpired = daysSourceExpired < 1 ? null : daysSourceExpired;
+		daysSourceExpired = daysSourceExpired == null || daysSourceExpired < 1 ? null : daysSourceExpired;
 
 		event.setResult(setExpirationDate(result, world, daysSourceExpired));
 	}
 
+	
+	/*
+	 * ON ITEM CRAFT
+	 */
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onPrepareItemCraft(PrepareItemCraftEvent event) {
 		plugin.debugInfo("onPrepareItemCraft");
@@ -64,6 +71,8 @@ public class ExpirationListener implements Listener {
 		if (result == null) {
 			plugin.debugInfo("onPrepareItemCraft result == null");
 			return;
+		}else{
+			plugin.debugInfo("onPrepareItemCraft result = " + event.getInventory().getResult().getType().name());
 		}
 
 		final World world = event.getViewers().get(0).getWorld();
@@ -75,11 +84,17 @@ public class ExpirationListener implements Listener {
 		// if the food is not expired set daysSourceExpired to null in case it
 		// is 0, we want setExpirationDate() to ignore it
 		Long daysSourceExpired = optionalLong != null && optionalLong.isPresent() ? optionalLong.getAsLong() : null;
-		daysSourceExpired = daysSourceExpired < 1 ? null : daysSourceExpired;
+		daysSourceExpired = daysSourceExpired == null || daysSourceExpired < 1 ? null : daysSourceExpired;
+		
+		plugin.debugInfo("onPrepareItemCraft daysSourceExpired = " + (daysSourceExpired == null? "null" : daysSourceExpired));
 
 		event.getInventory().setResult(setExpirationDate(result, world, daysSourceExpired));
 	}
 
+	
+	/*
+	 * ON PLAYER PICK UP
+	 */
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onPlayerPickupItem(PlayerPickupItemEvent event) {
 
@@ -94,7 +109,113 @@ public class ExpirationListener implements Listener {
 
 		item.setItemStack(setExpirationDate(item.getItemStack(), item.getWorld(), null));
 	}
+	
+	
+	/*
+	 * ON INVENTORY CLICK
+	 */
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onInventoryClickEvent(InventoryClickEvent event) {
 
+		if (!(event.getWhoClicked() instanceof Player)) {
+			return;
+		}
+
+		if (event.getCurrentItem() == null || event.getCurrentItem().getType().equals(Material.AIR)) {
+			return;
+		}
+
+		Player player = ((Player) event.getWhoClicked());
+		
+		if(plugin.getConfig().getBoolean(ConfigKeyEnum.SET_EXPIRATION_ON_INVENTORY_CLICK.getKey(), true)){
+			event.setCurrentItem(setExpirationDate(event.getCurrentItem(), player.getWorld(), null));
+		}
+
+		Long daysExpired = getDaysExpired(event.getCurrentItem(), player.getWorld());
+
+		if (daysExpired != null && daysExpired > 0) {
+			int lifeSpanInDays = getLifeSpanIndays(event.getCurrentItem().getType());
+			int buffMagnitude = getBuffMagnitude(daysExpired, lifeSpanInDays);
+
+			event.getWhoClicked()
+					.sendMessage(ChatColor.RED + "EXPIRED: " + daysExpired + " days! Danger level: " + buffMagnitude);
+		}
+	}
+
+	
+	/*
+	 * ON FOOD EAT
+	 */
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
+	public void onFoodEat(PlayerItemConsumeEvent event) {
+
+		Long daysExpired = getDaysExpired(event.getItem(), event.getPlayer().getWorld());
+
+		if (daysExpired == null || daysExpired < 1)
+			return;
+
+		int lifeSpanInDays = getLifeSpanIndays(event.getItem().getType());
+
+		int buffMagnitude = getBuffMagnitude(daysExpired, lifeSpanInDays);
+
+		Long buffDuration = plugin.getConfig().getLong(ConfigKeyEnum.BASE_DEBUFF_DURATION_IN_SECONDS.getKey(), 30);
+		buffDuration = buffDuration * (1 + (daysExpired / lifeSpanInDays));
+
+		plugin.debugInfo("onFoodEat buffMagnitude: " + buffMagnitude);
+		plugin.debugInfo("onFoodEat buffDuration: " + buffDuration);
+
+		if(plugin.getConfig().getBoolean(ConfigKeyEnum.BLINDING_ENABLED.getKey(), true)){
+			event.getPlayer().addPotionEffect(
+					new PotionEffect(PotionEffectType.BLINDNESS, buffDuration.intValue() * 20, buffMagnitude));
+		}
+		
+		if(plugin.getConfig().getBoolean(ConfigKeyEnum.CONFUSION_ENABLED.getKey(), true)){
+			event.getPlayer().addPotionEffect(
+					new PotionEffect(PotionEffectType.CONFUSION, buffDuration.intValue() * 20, buffMagnitude));
+		}
+
+		
+		if(plugin.getConfig().getBoolean(ConfigKeyEnum.DECREASE_FOOD_LEVEL_ENABLED.getKey(), true)){
+			Double newFoodLevel = event.getPlayer().getFoodLevel() * (1.0 / buffMagnitude);
+			newFoodLevel = newFoodLevel < 0 ? 0 : newFoodLevel;
+			event.getPlayer().setFoodLevel(newFoodLevel.intValue());
+		}
+		
+		if(plugin.getConfig().getBoolean(ConfigKeyEnum.DECREASE_SATURATION_ENABLED.getKey(), true)){
+			if (event.getPlayer().getSaturation() > 0) {
+				event.getPlayer().setSaturation(event.getPlayer().getSaturation() * (1.0f / buffMagnitude));
+			}
+		}
+		
+		if(plugin.getConfig().getBoolean(ConfigKeyEnum.HARM_ENABLED.getKey(), true)){
+			Long ageBeforeHarmProportion = plugin.getConfig()
+					.getLong(ConfigKeyEnum.AGE_BEFORE_HARM_AS_PROPORTION_OF_SHELF_LIFE.getKey(), 3);
+			plugin.debugInfo("onFoodEat ageBeforeHarmProportion: " + ageBeforeHarmProportion);
+			plugin.debugInfo(
+					"onFoodEat lifeSpanInDays*ageBeforeHarmProportion: " + lifeSpanInDays * ageBeforeHarmProportion);
+			if (lifeSpanInDays * ageBeforeHarmProportion <= daysExpired) {
+				int harmMagnifier = buffMagnitude;
+				if(ageBeforeHarmProportion <= buffMagnitude){
+					harmMagnifier = buffMagnitude/(ageBeforeHarmProportion.intValue());
+				}else if(buffMagnitude > 1){
+					buffMagnitude = buffMagnitude/2;
+				}
+				if(ageBeforeHarmProportion <= buffDuration){
+					buffDuration = buffDuration/(ageBeforeHarmProportion.intValue());
+				}else if(buffDuration > 1){
+					buffDuration = buffDuration/2;
+				}
+				event.getPlayer().addPotionEffect(
+						new PotionEffect(PotionEffectType.HARM, buffDuration.intValue() * 20, harmMagnifier));
+			}
+		}
+		
+	}
+
+	
+	/*
+	 * SET EXPIRATION DATE
+	 */
 	private ItemStack setExpirationDate(ItemStack itemStack, World world, Long daysExpired) {
 		plugin.debugInfo("setExpirationDate");
 
@@ -165,100 +286,11 @@ public class ExpirationListener implements Listener {
 		return itemStack;
 	}
 
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onInventoryClickEvent(InventoryClickEvent event) {
+	
 
-		if (!(event.getWhoClicked() instanceof Player)) {
-			return;
-		}
-
-		if (event.getCurrentItem() == null || event.getCurrentItem().getType().equals(Material.AIR)) {
-			return;
-		}
-
-		Player player = ((Player) event.getWhoClicked());
-		
-		if(plugin.getConfig().getBoolean(ConfigKeyEnum.SET_EXPIRATION_ON_INVENTORY_CLICK.getKey(), true)){
-			event.setCurrentItem(setExpirationDate(event.getCurrentItem(), player.getWorld(), null));
-		}
-
-		Long daysExpired = getDaysExpired(event.getCurrentItem(), player.getWorld());
-
-		if (daysExpired != null && daysExpired > 0) {
-			int lifeSpanInDays = getLifeSpanIndays(event.getCurrentItem().getType());
-			int buffMagnitude = getBuffMagnitude(daysExpired, lifeSpanInDays);
-
-			event.getWhoClicked()
-					.sendMessage(ChatColor.RED + "EXPIRED: " + daysExpired + " days! Danger level: " + buffMagnitude);
-		}
-	}
-
-	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
-	public void onFoodEat(PlayerItemConsumeEvent event) {
-
-		Long daysExpired = getDaysExpired(event.getItem(), event.getPlayer().getWorld());
-
-		if (daysExpired == null || daysExpired < 1)
-			return;
-
-		int lifeSpanInDays = getLifeSpanIndays(event.getItem().getType());
-
-		int buffMagnitude = getBuffMagnitude(daysExpired, lifeSpanInDays);
-
-		Long buffDuration = plugin.getConfig().getLong(ConfigKeyEnum.BASE_DEBUFF_DURATION_IN_SECONDS.getKey(), 30);
-		buffDuration = buffDuration * (1 + (daysExpired / lifeSpanInDays));
-
-		plugin.debugInfo("onFoodEat buffMagnitude: " + buffMagnitude);
-		plugin.debugInfo("onFoodEat buffDuration: " + buffDuration);
-
-		if(plugin.getConfig().getBoolean(ConfigKeyEnum.BLINDING_ENABLED.getKey(), true)){
-			event.getPlayer().addPotionEffect(
-					new PotionEffect(PotionEffectType.BLINDNESS, buffDuration.intValue() * 20, buffMagnitude));
-		}
-		
-		if(plugin.getConfig().getBoolean(ConfigKeyEnum.CONFUSION_ENABLED.getKey(), true)){
-			event.getPlayer().addPotionEffect(
-					new PotionEffect(PotionEffectType.CONFUSION, buffDuration.intValue() * 20, buffMagnitude));
-		}
-
-		
-		if(plugin.getConfig().getBoolean(ConfigKeyEnum.DECREASE_FOOD_LEVEL_ENABLED.getKey(), true)){
-			Double newFoodLevel = event.getPlayer().getFoodLevel() * (1.0 / buffMagnitude);
-			newFoodLevel = newFoodLevel < 0 ? 0 : newFoodLevel;
-			event.getPlayer().setFoodLevel(newFoodLevel.intValue());
-		}
-		
-		if(plugin.getConfig().getBoolean(ConfigKeyEnum.DECREASE_SATURATION_ENABLED.getKey(), true)){
-			if (event.getPlayer().getSaturation() > 0) {
-				event.getPlayer().setSaturation(event.getPlayer().getSaturation() * (1.0f / buffMagnitude));
-			}
-		}
-		
-		if(plugin.getConfig().getBoolean(ConfigKeyEnum.HARM_ENABLED.getKey(), true)){
-			Long ageBeforeHarmProportion = plugin.getConfig()
-					.getLong(ConfigKeyEnum.AGE_BEFORE_HARM_AS_PROPORTION_OF_SHELF_LIFE.getKey(), 3);
-			plugin.debugInfo("onFoodEat ageBeforeHarmProportion: " + ageBeforeHarmProportion);
-			plugin.debugInfo(
-					"onFoodEat lifeSpanInDays*ageBeforeHarmProportion: " + lifeSpanInDays * ageBeforeHarmProportion);
-			if (lifeSpanInDays * ageBeforeHarmProportion <= daysExpired) {
-				int harmMagnifier = buffMagnitude;
-				if(ageBeforeHarmProportion <= buffMagnitude){
-					harmMagnifier = buffMagnitude/(ageBeforeHarmProportion.intValue());
-				}else if(buffMagnitude > 1){
-					buffMagnitude = buffMagnitude/2;
-				}
-				if(ageBeforeHarmProportion <= buffDuration){
-					buffDuration = buffDuration/(ageBeforeHarmProportion.intValue());
-				}else if(buffDuration > 1){
-					buffDuration = buffDuration/2;
-				}
-				event.getPlayer().addPotionEffect(
-						new PotionEffect(PotionEffectType.HARM, buffDuration.intValue() * 20, harmMagnifier));
-			}
-		}
-		
-	}
-
+	/*
+	 * GET LIFESPAN
+	 */
 	private int getLifeSpanIndays(Material material) {
 		Optional<String> lifeSpan = plugin.getConfig().getStringList(ConfigKeyEnum.LIFE_SPANS.getKey()).stream()
 				.filter(l -> l.startsWith(material.name())).findFirst();
@@ -273,12 +305,20 @@ public class ExpirationListener implements Listener {
 		return lifeSpanInDays;
 	}
 
+	
+	/*
+	 * GET BUFF MAGNITUDE
+	 */
 	private int getBuffMagnitude(long daysExpired, int lifeSpanInDays) {
 		Long additionalMagnitude = (daysExpired / ((lifeSpanInDays < 4 ? 4 : lifeSpanInDays) / 4));
 		int buffMagnitude = 1 + additionalMagnitude.intValue();
 		return buffMagnitude;
 	}
 
+	
+	/*
+	 * GET DAYS EXPIRED
+	 */
 	private long getDaysExpired(ItemStack itemStack, World world) {
 
 		if (itemStack == null) {
